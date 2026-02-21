34e4679f0274479ed62304a65639126dec5479fd
supaat <supaat.wi@gmai.com>
2026-02-21 16:59:53 +0700

    fix: error support anyhow

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use zip::result::ZipError;
 
 
@@ -21,3 +23,17 @@ impl From<ZipError> for MrXlsxError {
 		MrXlsxError::ZipError(e.to_string())
 	}
 }
+
+
+impl fmt::Display for MrXlsxError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MrXlsxError::Io(e) => write!(f, "IO error: {e}"),
+			MrXlsxError::AlreadyExists(e) => write!(f, "Already Exists Sheet : {e}"),
+			MrXlsxError::NotFound(e) => write!(f, "Mr Xlsx Not Found : {e}"),
+			MrXlsxError::ZipError(e) => write!(f, "Zip Error : {e}")
+        }
+    }
+}
+
+impl std::error::Error for MrXlsxError {}
